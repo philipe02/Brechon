bd25484534d74fc605c85b10f5dad997cfd97276
Philipe Santos <philipe_02@hotmail.com>
2021-02-27 22:25:41 -0300

    Primeiro commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Testando integração com o celular</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    fontSize: 18,
+    textAlignVertical: 'center',
+    color: '#ddd'
+  }
+});
---
+++ README.md
@@ -0,0 +1,19 @@
+# Brechon
+
+Uma aplicação feita em React Native para praticar.
+
+### Instalando e rodando a aplicação
+
+**Instalando dependências**
+
+```
+yarn install
+```
+
+**Rodando a aplicação**
+
+```
+yarn start
+```
+
+E usar no QR code no aplicativo [Expo Go](https://play.google.com/store/apps/details?id=host.exp.exponent&hl=en&gl=US) para rodar a aplicação no celular ou rodar a aplicação em um browser
